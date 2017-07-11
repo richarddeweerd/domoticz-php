c4a80e8664b4fd144a7255bae87efd23fffe7279
Richard de Weerd <richard@deweerd.biz>
2017-07-11 22:13:23 +0200

    fixed sethue

--- php/pass2php.php
+++ php/pass2php.php
@@ -45,7 +45,7 @@ if(apcu_fetch('cron5')<time-4){
 	include('/opt/jarvis/php/_cron5.php');
 }
 function sethue($name,$hue,$bright,$comment=''){
-	$msg = 'SetRGB '.$name.' => '.$rgb;
+	$msg = 'SetRGB '.$name.' => '.$hue.' | '.$bright;
 	if(!empty($comment)) $msg.=' => '.$comment;
 	$msg = 	'http://192.168.1.200:8080/json.htm?type=command&param=setcolbrightnessvalue&idx='.apcu_fetch('i'.$name).'&hue='.$hue,'&brightness='.$bright.'&iswhite=false';
 	lg($msg);
