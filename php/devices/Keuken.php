8c5f58b1828bd72bcd676f1aedd18c1dea56f4e9
Richard de Weerd <richard@deweerd.biz>
2017-08-24 23:29:32 +0200

    Keuken Dimmer

--- php/devices/Keuken.php
+++ php/devices/Keuken.php
@@ -2,7 +2,7 @@
 if($status!="Off"){
   //notify("Help","Overloop");
   //sethue("Bed_RGBW",220,40);
-  sethue("AppLamp_Keuken",128,40,true);
+  if(apcu_exists('sKeuken'))setdimmer("AppLamp_Keuken",apcu_fetch('sKeuken'));
 }else{
   sw("AppLamp_Keuken","Off")
 }
