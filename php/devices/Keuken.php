1dda21cfaa669a73336dc9c93872aa328ef22653
Richard de Weerd <richard@deweerd.biz>
2017-08-25 00:14:45 +0200

    status

--- php/devices/Keuken.php
+++ php/devices/Keuken.php
@@ -3,7 +3,7 @@ if($status!="Off"){
   //notify("Help","Overloop");
   //sethue("Bed_RGBW",220,40);
   if(apcu_exists('sKeuken')){
-    setdimmer("AppLamp_Keuken",apcu_fetch('sKeuken'));
+    setdimmer("AppLamp_Keuken",$status);
   } else {}
 
 
