f3cc7757b84de2f695604888a00a44ba872d9167
Richard de Weerd <richard@deweerd.biz>
2017-08-24 23:57:21 +0200

    Bugfix keukendimmer

--- php/devices/Keuken.php
+++ php/devices/Keuken.php
@@ -2,7 +2,11 @@
 if($status!="Off"){
   //notify("Help","Overloop");
   //sethue("Bed_RGBW",220,40);
-  if(apcu_exists('sKeuken'))setdimmer("AppLamp_Keuken",apcu_fetch('sKeuken'));
+  if(apcu_exists('sKeuken')){
+    setdimmer("AppLamp_Keuken",apcu_fetch('sKeuken'));
+  } else {}
+
+
 }else{
-  sw("AppLamp_Keuken","Off")
+  sw("AppLamp_Keuken","Off");
 }
