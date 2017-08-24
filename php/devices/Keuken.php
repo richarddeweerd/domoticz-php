e5bb398ea723f90d97f1e1e7c84a3abe3dcd29f1
Richard de Weerd <richard@deweerd.biz>
2017-08-25 00:46:34 +0200

    de

--- php/devices/Keuken.php
+++ php/devices/Keuken.php
@@ -1,6 +1,14 @@
 <?php
 if($status!="Off"){
-    setdimmer("AppLamp_Keuken",$status);
+    if ($status=="On"){
+      sw("AppLamp_Keuken","On");
+    } else {
+      $s = explode(" ",$status);
+      if ($s[2] == "Level:") {
+        setdimmer("AppLamp_Keuken",$s[3]);
+      }  
+    }
+    
 
 }else{
   sw("AppLamp_Keuken","Off");
