48a37f0b464a16c1ff7fd3f19eeef3576263ee0f
Richard de Weerd <richard@deweerd.biz>
2017-09-04 20:59:47 +0200

    Gordijnen fix

--- php/devices/Gordijnen.php
+++ php/devices/Gordijnen.php
@@ -1,12 +1,12 @@
 <?php
 lg($status);
 
-if($status=="Off"){
+if($status=="Open"){
   #open
   sw("Gordijnen_Openen",'On');
   sw("Gordijnen_Openen",'Off');
 
-}elseif ($status=="On") {
+}elseif ($status=="Closed") {
   # Close
   sw("Gordijnen_Sluiten",'On');
   sw("Gordijnen_Sluiten",'Off');
