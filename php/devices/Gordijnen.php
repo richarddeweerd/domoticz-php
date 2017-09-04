c1b40329ab15492b74494f4678be5e2342061425
Richard de Weerd <richard@deweerd.biz>
2017-09-04 20:49:16 +0200

    G2

--- php/devices/Gordijnen.php
+++ php/devices/Gordijnen.php
@@ -2,8 +2,15 @@
 lg($status);
 
 if($status=="Off"){
+  #open
+  sw("Gordijnen_Openen",'On');
+  sw("Gordijnen_Openen",'Off');
 
+}elseif ($status=="On") {
+  # Close
+  sw("Gordijnen_Sluiten",'On');
+  sw("Gordijnen_Sluiten",'Off');
 
-}else{
+} else{
 
 }
--- php/devices/Overloop.php
+++ php/devices/Overloop.php
@@ -1,5 +1,4 @@
 <?php
 if($status=="On"){
   //notify("Help","Overloop");
-  //sethue("Bed_RGBW",220,40);
 }
