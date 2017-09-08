8a31041aee151cb4fbdd6113c07c467bee18eab8
Richard de Weerd <richard@deweerd.biz>
2017-09-08 09:51:12 +0200

    BF2

--- php/devices/Alarm_Burg.php
+++ php/devices/Alarm_Burg.php
@@ -1,9 +1,9 @@
 <?php
 if($status=="Open"){
-  sethue("sBed_RGBW",1,100,false);
+  sethue("sBed_RGBW",1,100,"false");
   notify("Alarm","Inbraak Alarm");
 }
 
 if($status=="Closed"){
-  sethue("sBed_RGBW",128,40,false);
+  sethue("sBed_RGBW",128,40,"false";
 }
--- php/devices/Armed_Home.php
+++ php/devices/Armed_Home.php
@@ -1,7 +1,7 @@
 <?php
 if($status=="Open"){
-  sethue("Bed_RGBW",220,40,false);
+  sethue("Bed_RGBW",220,40,"false");
   notify("Alarm","Armed Home");
 } else if ($status=="Closed"){
-  sethue("Bed_RGBW",128,40,false);
+  sethue("Bed_RGBW",128,40,"false");
 }
