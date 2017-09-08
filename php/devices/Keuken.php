955fce5d583766c371cc87546844c83cf86d3f96
Richard de Weerd <richard@deweerd.biz>
2017-09-08 21:18:49 +0200

    Added override

--- php/devices/Keuken.php
+++ php/devices/Keuken.php
@@ -1,22 +1,21 @@
 <?php
-
-lg($status); 
-        
+       
 if($status!="Off"){
     if ($status=="On"){
+      @include '/opt/jarvis/php/devices/_check_keukendimmer.php';
     } else {
       $s = explode(" ",$status);
       if ($s[1] == "Level:") {
-        //setdimmer("AppLamp_Keuken",$s[2]);
-//        lg($s[2]); 
-        
-        //apcu_fetch('vvKeuken_Dimmer_High')
+        if (($s[2] != apcu_fetch('vvKeuken_Dimmer_High'))&& ($s[2] != apcu_fetch('vvKeuken_Dimmer_Low')) ){
+          //Override
+          setdimmer("Set_Keukendimmer", 30);
+        }
       }  
     }
     
 
 }else{
-  sw("AppLamp_Keuken","Off");
+  //sw("AppLamp_Keuken","Off");
   if (apcu_fetch('sSet_Keukendimmer')== "Override"){
     setdimmer("Set_Keukendimmer", 20);
   }
