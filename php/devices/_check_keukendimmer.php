cf1ea0405fd05432ab18178b174e3722da872b8d
Richard de Weerd <richard@deweerd.biz>
2017-09-08 21:55:59 +0200

    Bugfix

--- php/devices/_check_keukendimmer.php
+++ php/devices/_check_keukendimmer.php
@@ -5,17 +5,15 @@ switch (apcu_fetch('sSet_Keukendimmer')) {
         break;
     case "Auto":
         //Auto
-        if (apcu_fetch('sKeuken_PIR')=='On'){
-            //Dimmer high
-            setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_High'));
-        } elseif (apcu_fetch('sKeuken_PIR')=='Off'){
-            //Dimmer low
-            setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_Low'));
-        }        
-        break;
-    case "Override":
-        //Auto
-
+        if (apcu_fetch('OverrideKeukenDimmer') != "True"){
+            if (apcu_fetch('sKeuken_PIR')=='On'){
+                //Dimmer high
+                setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_High'));
+            } elseif (apcu_fetch('sKeuken_PIR')=='Off'){
+                //Dimmer low
+                setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_Low'));
+            }        
+        }
         break;
 }
 ?>
