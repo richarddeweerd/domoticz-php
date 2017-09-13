c4bbd9fe96bd25bffe87175087dec14f268d8bc3
Richard de Weerd <richard@deweerd.biz>
2017-09-13 22:04:56 +0200

    Changed wakeup to applamp

--- php/devices/Keuken_PIR.php
+++ php/devices/Keuken_PIR.php
@@ -1,4 +1,14 @@
 <?php
 apcu_store('s'.$device,$status);
+if ($status == "On"){
+    if ((int)apcu_fetch('sKeuken_Lux') <= 1){
+        //Low light and PIR registers movement -> wakeup
+        apcu_store('WakeupKeukenDimmer',"True");
+        sethue("AppLamp",128,80,"false");
+    }
+} elseif ($status == "Off"){
+    apcu_store('WakeupKeukenDimmer',"False");
+    sw("AppLamp","Off");
+}
 @include '/opt/jarvis/php/devices/_check_keukendimmer.php';
 ?>
--- php/devices/_check_keukendimmer.php
+++ php/devices/_check_keukendimmer.php
@@ -5,33 +5,16 @@ switch (apcu_fetch('sSet_Keukendimmer')) {
         break;
     case "Auto":
         //Auto
-        if ( apcu_fetch('sKeuken')=="Off"){
-            //Light is out check for wakeup
-            if (((int)apcu_fetch('sKeuken_Lux') <=1)&&(apcu_fetch('sKeuken_PIR')=="On")){
-                //Low light and PIR registers movement -> wakeup
-                apcu_store('WakeupKeukenDimmer',"True");
-                setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_Low'));
-            }
-        } else {
+        if ( apcu_fetch('sKeuken')!="Off"){
             //Light is on
-            if (apcu_fetch('WakeupKeukenDimmer')=="True"){
-                //Wakeup is active
-                if (apcu_fetch('sKeuken_PIR')=="Off"){
-                    //End wakeup
-                    apcu_store('WakeupKeukenDimmer',"True");
-                    sw("Keuken","Off");
-                }
-            } else {
-                //Normal operation
-                if (apcu_fetch('OverrideKeukenDimmer') != "True"){
-                    //No overide active check for PIR
-                    if (apcu_fetch('sKeuken_PIR')=='On'){
-                        //Dimmer high
-                        setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_High'));
-                    } elseif (apcu_fetch('sKeuken_PIR')=='Off'){
-                        //Dimmer low
-                        setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_Low'));
-                    }        
+            if (apcu_fetch('OverrideKeukenDimmer') != "True"){
+                //No overide active check for PIR
+                if (apcu_fetch('sKeuken_PIR')=='On'){
+                    //Dimmer high
+                    setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_High'));
+                } elseif (apcu_fetch('sKeuken_PIR')=='Off'){
+                    //Dimmer low
+                    setdimmer("Keuken",apcu_fetch('vvKeuken_Dimmer_Low'));
                 }        
             }
         }
